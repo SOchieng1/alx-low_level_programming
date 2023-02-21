633e6eaf98ecbbb59837dfe6985001b686839999
SOchieng1 <sharonochieng28>
2023-02-21 05:39:12 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		_putchar (s);
+	}
+	_putchar('\n');
+}
+
